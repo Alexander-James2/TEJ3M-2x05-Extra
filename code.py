6e03ada5e88a6c0f1e5de478e0fad0b4bfed7b43
Alexander-James2 <144252140+Alexander-James2@users.noreply.github.com>
2023-11-03 10:43:50 -0400

    Update and rename main.py to code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+# Created by Alexander James
+# Created on Nov. 2023
+
+from time import sleep
+from machine import Pin, PWM
+
+pwm = PWM(Pin(1))
+pwm.freq(50)
+
+while True:
+    for position in range(1000,9000,50):
+        pwm.duty_u16(position)
+        sleep(0.01)
+    for position in range(9000,1000,-50):
+        pwm.duty_u16(position)
+        sleep(0.01)
--- main.py
+++
@@ -1,2 +0,0 @@
-# Created by Alexander James
-# Created on Nov. 2023
